41f4b671a6444475a75ce17c93bc1013cc9961c4
JeremieN6 <ngoyi.jeremie@gmail.com>
2024-06-14 14:43:55 +0200

    Custom Admin Controller. Ajout de la possibillité de voir la catégorie de l'article. Ajout des dates de Création & modification. Masquer slug et update dans la vue principal des posts.

--- src/Controller/Admin/PostsCrudController.php
+++ src/Controller/Admin/PostsCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Posts;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -23,10 +24,13 @@ class PostsCrudController extends AbstractCrudController
         return [
             IdField::new('id')->hideOnForm(),
             TextField::new('title'),
-            SlugField::new('slug')->setTargetFieldName('title'),
+            SlugField::new('slug')->setTargetFieldName('title')->hideOnIndex(),
             TextEditorField::new('content'),
             TextField::new('featuredImage'),
             AssociationField::new('users'),
+            AssociationField::new('categories'),
+            DateField::new('createdAt'),
+            DateField::new('updatedAt')->hideOnIndex()
         ];
     }
     
